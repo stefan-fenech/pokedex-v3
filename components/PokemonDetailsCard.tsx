f3813e7b3382404b93ee9fd68bc72641c59b6ce2
Stefan <stefan.fenech@live.com.au>
2022-11-07 20:51:36 +1100

    More null assertions

--- components/PokemonDetailsCard.tsx
+++ components/PokemonDetailsCard.tsx
@@ -23,13 +23,13 @@ export function PokemonDetails({ id, name, type, type2, image }: PokemonDetailsP
             <div className='flex flex-col bg-white m-4 rounded-md p-4'>
                 <div className='mt-4'>
                     <div className='flex justify-center text-white'>
-                        <p className={`${typeColorOne?.color} flex flex-row justify-center items-center mr-2 px-2 rounded-md border-2 border-white capitalize`}>
-                            <Image src={typeColorOne?.icon} alt='type' width='32' height='32' />
+                        <p className={`${typeColorOne!.color} flex flex-row justify-center items-center mr-2 px-2 rounded-md border-2 border-white capitalize`}>
+                            <Image src={typeColorOne!.icon} alt='type' width='32' height='32' />
                             {type}
                         </p>
                         {type2 && (
-                            <p className={`${typeColorTwo?.color} flex flex-row justify-center items-center mr-2 px-2 rounded-md border-2 border-white capitalize`}>
-                                <Image src={typeColorTwo?.icon} alt='type' width='32' height='32' />
+                            <p className={`${typeColorTwo!.color} flex flex-row justify-center items-center mr-2 px-2 rounded-md border-2 border-white capitalize`}>
+                                <Image src={typeColorTwo!.icon} alt='type' width='32' height='32' />
                                 {type2}
                             </p>
                         )}
