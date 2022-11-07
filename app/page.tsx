9c6bcc20a91a87df9e5389a5814c3297b6b93cb2
Stefan <stefan.fenech@live.com.au>
2022-11-07 20:49:58 +1100

    null assertion operator to PokemonCard.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ async function getPokemonData(pokemonList: any[]) {
 }
 
 async function PokemonPage() {
-    const pokemons = await getPokemonList(10);
+    const pokemons = await getPokemonList(151);
     const pokemonData = await getPokemonData(pokemons);
 
     return (
--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -15,20 +15,20 @@ function PokemonCard({ name, id, type, type2, image }: PokemonCardProps) {
 
     return (
         <div className='flex flex-row w-96 h-32'>
-            <div className={`${typeColorOne?.color} h-full rounded-md text-black w-full p-2 border-2 border-gray-300`}>
+            <div className={`${typeColorOne!.color} h-full rounded-md text-black w-full p-2 border-2 border-gray-300`}>
                 <div className='flex flex-row h-full justify-between'>
                     <div className='flex flex-col justify-between gap-1 text-gray-800'>
                         <h2 className='text-xl'>{`#${id}`}</h2>
                         <h1 className='text-2xl capitalize'>{name}</h1>
                         <div>
                             <div className='flex text-white'>
-                                <p className={`${typeColorOne?.color} flex flex-row justify-center items-center mr-2 px-2 rounded-md border-2 border-white capitalize`}>
-                                    <Image src={typeColorOne?.icon} alt='type' width='32' height='32' />
+                                <p className={`${typeColorOne!.color} flex flex-row justify-center items-center mr-2 px-2 rounded-md border-2 border-white capitalize`}>
+                                    <Image src={typeColorOne!.icon} alt='type' width='32' height='32' />
                                     {type}
                                 </p>
                                 {type2 && (
-                                    <p className={`${typeColorTwo?.color} flex flex-row justify-center items-center mr-2 px-2 rounded-md border-2 border-white capitalize`}>
-                                        <Image src={typeColorTwo?.icon} alt='type' width='32' height='32' />
+                                    <p className={`${typeColorTwo!.color} flex flex-row justify-center items-center mr-2 px-2 rounded-md border-2 border-white capitalize`}>
+                                        <Image src={typeColorTwo!.icon} alt='type2' width='32' height='32' />
                                         {type2}
                                     </p>
                                 )}
